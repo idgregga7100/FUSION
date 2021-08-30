d54284497d3c96963183980588fd298753f3b754
isabelle7100 <57689906+isabelle7100@users.noreply.github.com>
2021-08-30 12:30:46 -0500

    Create combine_FUSIONout_chr_files.R

---
+++ combine_FUSIONout_chr_files.R
@@ -0,0 +1,24 @@
+#FUSION runs one chr at a time, so combine the separate chromosome files into single phenotype files. mhc region is also run separately
+#example data from Schlidkraut cancer sumstats
+
+library(data.table)
+library(dplyr)
+"%&%" = function(a,b) paste(a,b,sep="")
+
+phenos<-c('breast_erneg','breast_erpos','breast','endo','ovary','ovary_hgsoc','prostate')
+#phenos<-c('breast_erneg')
+
+#read in chr1, add on each following chr file and mhc
+for (pheno in phenos){
+  file1<-fread('/home/isabelle/FUSION/fusion_twas-master/cancer_pwas/'%&%pheno%&%'_hg38_chr1.dat',header=T,stringsAsFactors=F)
+  for (n in 2:22){
+    file2<-fread('/home/isabelle/FUSION/fusion_twas-master/cancer_pwas/'%&%pheno%&%'_hg38_chr'%&%n%&%'.dat',header=T,stringsAsFactors=F)
+    print(pheno%&%' chr'%&%n%&%' with '%&%nrow(file2)%&%' genes')
+    file1<-rbind(file1,file2)
+  }
+  mhc<-fread('/home/isabelle/FUSION/fusion_twas-master/cancer_pwas/'%&%pheno%&%'_hg38_chr6.dat.MHC',header=T,stringsAsFactors=F)
+  print(pheno%&%'MHC with '%&%nrow(mhc)%&%' genes')
+  file1<-rbind(file1,mhc)
+  print(pheno%&%' with '%&%nrow(file1)%&%' total genes')
+  write.csv(file1,'/home/isabelle/FUSION/fusion_twas-master/cancer_pwas/'%&%pheno%&%'_FUSION_PWAS.csv',quote=F,row.names=F)
+}
