b2305e4bde26520757013fc355d6431f1c7a852f
isabelle7100 <57689906+isabelle7100@users.noreply.github.com>
2021-08-31 13:41:15 -0500

    Create pull_genes.sh

---
+++ pull_genes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#pull lines with specific genes of interest
+
+for gene in KDELC2 GSTM1 RSPO3 PLG ARL3 MMP10 ABO
+do
+ grep $gene FUSIONPWAS_all_pheno.csv > pulled_$gene.csv
+done
+
+cat pulled_KDELC2.csv pulled_GSTM1.csv pulled_RSPO3.csv pulled_PLG.csv pulled_ARL3.csv pulled_MMP10.csv pulled_ABO.csv > FUSION_PWAS_pulled_results.csv
+
+#this won't have colnames tho so have to go back and add those. could i have done this with R? sure probably. but this was easier
